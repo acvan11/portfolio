9cfef5f8c13c5ef9c6192fc10e4e14e5689bf0cf
Sky <anvandp18@gmail.com>
2018-09-26 23:12:29 -0700

    create some components

---
+++ src/components/main.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import LandingPage from './LandingPage';
+
+const Main = () => {
+	<Switch>
+		<Route exact path="/" component={LandingPage}>
+	</Switch>
+}
+
+export default Main;
