5203cd55585f544285156ffef4838366e1359f0c
Sky <anvandp18@gmail.com>
2018-09-27 05:50:55 -0700

    have the tab contact, resume, aboutme, project

--- src/components/main.js
+++ src/components/main.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 
-import LandingPage from './LandingPage';
+import Landing from './landingpage';
+import About from './aboutme';
+import Contact from './contact';
+import Resume from './resume';
+import Projects from './projects';
 
-const Main = () => {
+const Main = () => (
 	<Switch>
-		<Route exact path="/" component={LandingPage}>
+		<Route exact path="/" component={Landing} />
+		<Route exact path="/about" component={About} />
+		<Route exact path="/contact" component={Contact} />
+		<Route exact path="/resume" component={Resume} />
+		<Route exact path="/projects" component={Projects} />
 	</Switch>
-}
+)
 
 export default Main;
